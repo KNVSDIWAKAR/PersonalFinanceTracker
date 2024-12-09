8ec5b94b10957af9223bf5ad5a326207128af02d
KNVSDIWAKAR <divvukancherla630@gmail.com>
2024-12-09 22:31:12 +0530

    Modifed The Frontend Links

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://personal-finance-tracker-frontend-azure.vercel.app",
     methods: ["POST", "GET"],
   })
 );
